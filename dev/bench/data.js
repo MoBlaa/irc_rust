d2e186170ac6a6f57edf41b806d7a8f33d73d4c4
github-action-benchmark <github@users.noreply.github.com>
2020-09-07 16:51:07 +0000

    add Benchmark (cargo) benchmark result for 9cf8e9829f1c1e5fa86141a4e464f7ab6d87d643

---
+++ dev/bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1599497467150,
+  "repoUrl": "https://github.com/MoBlaa/irc_rust",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "moblaa@pm.me",
+            "name": "moblaa"
+          },
+          "committer": {
+            "email": "moblaa@pm.me",
+            "name": "moblaa"
+          },
+          "distinct": true,
+          "id": "9cf8e9829f1c1e5fa86141a4e464f7ab6d87d643",
+          "message": "added personal token to benchmark uploads",
+          "timestamp": "2020-09-07T18:49:28+02:00",
+          "tree_id": "47c2c603c9b8437059ee1bd5c4048b839a2458dd",
+          "url": "https://github.com/MoBlaa/irc_rust/commit/9cf8e9829f1c1e5fa86141a4e464f7ab6d87d643"
+        },
+        "date": 1599497466782,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench__bench_params_create",
+            "value": 283,
+            "range": "± 49",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench__bench_params_iter",
+            "value": 2559,
+            "range": "± 29",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench__bench_parse",
+            "value": 1168,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench__bench_tag_create",
+            "value": 1764,
+            "range": "± 25",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench__bench_tag_index",
+            "value": 152,
+            "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
